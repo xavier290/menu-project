5d155a0aac6ff93072280df5d226dc292b090411
xavier290 <xa25alex@gmail.com>
2022-01-24 16:45:08 -0600

    project finished

---
+++ js/menu-data.js
@@ -0,0 +1,184 @@
+const menu = [
+    {
+        id: 1,
+        title: "Japanese katsu curry",
+        category: "Main",
+        desc: "Breaded chicken in a creamy katsu sauce, spring onion and chilli. Served with wok fried veg and noodles"
+    },
+    {
+        id: 2,
+        title: "Hoisin duck salad",
+        category: "Main",
+        desc: "Sweet and sticky shredded duck, chargrilled pineapple, carrots, cucumber, pickled slaw, pak choi and Chinese leaf with a soy and ginger dressing"
+    },
+    {
+        id: 3,
+        title: "Thai duck stir fry",
+        category: "Main",
+        desc: "Shredded duck with crispy onions, sweet chilli jam and Asian slaw in a teriyaki sauce. Served with wok fried veg and noodles."
+    },
+    {
+        id: 4,
+        title: "Pink Caesar salad",
+        category: "Main",
+        desc: "Baby gem, croutons, tomatoes, Parmigiano Reggiano crisps, beetroot, boiled egg and smoked Caesar salad dressing"
+    },
+    {
+        id: 5,
+        title: "Steak and chips",
+        category: "Main",
+        desc: "8oz British sirloin steak served with chipotle butter, roasted tomato, beer battered onion rings and lemon and pepper seasoned fries"
+    },
+    {
+        id: 6,
+        title: "Chicken swinging kebab",
+        category: "Main",
+        desc: "Grilled swinging kebab served with Greek salad, hummus and flatbread."
+    },
+    {
+        id: 7,
+        title: "Cod and chips",
+        category: "Main",
+        desc: "Tempura battered cod served with caramelised lemon, mushy peas and lemon and pepper seasoned fries"
+    },
+    {
+        id: 8,
+        title: "ARGENTINIAN RIBS",
+        category: "Main",
+        desc: "Pork ribs in a smoky spiced rub and chimichurri. Served with lemon and pepper seasoned fries and cabbage slaw"
+    },
+    {
+        id: 9,
+        title: "Korean BBQ chicken",
+        category: "Main",
+        desc: "Sticky hand-breaded chicken, chargrilled pineapple, grated carrots, slaw and spring onion"
+    },
+    {
+        id: 10,
+        title: "Warm chocolate brownie (v)",
+        category: "Dessert",
+        desc: "With melted chocolate, strawberries and vanilla ice cream"
+    },
+    {
+        id: 11,
+        title: "Apple crumble (vg)",
+        category: "Dessert",
+        desc: "Served with vegan vanilla ice cream"
+    },
+    {
+        id: 12,
+        title: "Churros (v)",
+        category: "Dessert",
+        desc: "Classic Spanish donuts coated in cinnamon sugar and served with chocolate dipping sauce"
+    },
+    {
+        id: 13,
+        title: "Ice-creams (v)",
+        category: "Dessert",
+        desc: "Three scoops of..."
+    },
+    {
+        id: 14,
+        title: "Sticky toffee pudding (v)",
+        category: "Dessert",
+        desc: "With vanilla ice cream"
+    },
+    {
+        id: 15,
+        title: "Fresh lemonade with mint",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 16,
+        title: "Sunshine coast iced tea",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 17,
+        title: "Strawberry smash",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 18,
+        title: "Wavebreaker",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 19,
+        title: "Smoothie blended with apple juice",
+        category: "Smoothies",
+        desc: " "
+    },
+    {
+        id: 20,
+        title: "Passionfruit",
+        category: "Smoothies",
+        desc: " "
+    },
+    {
+        id: 21,
+        title: "Kale",
+        category: "Smoothies",
+        desc: " "
+    },
+    {
+        id: 22,
+        title: "Central Monte Merlot",
+        category: "Wines",
+        desc: " "
+    },
+    {
+        id: 23,
+        title: "Le Jardin Cabernet Sauvignon",
+        category: "Wines",
+        desc: " "
+    },
+    {
+        id: 24,
+        title: "Famiglia botter prosecco rosé",
+        category: "Wines",
+        desc: " "
+    },
+    {
+        id: 25,
+        title: "Primi Soli Pinot Grigio",
+        category: "Wines",
+        desc: " "
+    },
+    {
+        id: 26,
+        title: "Azzurro",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 27,
+        title: "Corona",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 28,
+        title: "Gordons Gin and Tonic",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 29,
+        title: "Tanqueray Gin and Tonic",
+        category: "Drinks",
+        desc: " "
+    },
+    {
+        id: 30,
+        title: "Bodega Bay Elderflower, Lemon and Mint",
+        category: "Drinks",
+        desc: " "
+    },
+]
+
+export default menu;
